133a9241443b4c0525dcf9ceecb357cabbcd3cab
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-11-10 15:50:19 +0700

    Update basic test

--- apps/contracts/test/GregDao.ts
+++ apps/contracts/test/GregDao.ts
@@ -3,8 +3,9 @@ import { expect } from 'chai'
 import hre from 'hardhat'
 
 const deploy = async () => {
-  const contract = await hre.viem.deployContract('Contract', [
-    'Contract', // _name
+  const contract = await hre.viem.deployContract('GregToken', [
+    '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', // initialOwner (default hardhat account)
+    '0xD4416b13d2b3a9aBae7AcD5D6C2BbDBE25686401', // nameWrapperAddress (mainnet deployment)
   ])
 
   return { contract }
@@ -15,6 +16,6 @@ describe('Tests', function () {
     const { contract } = await loadFixture(deploy)
 
     const contractName = await contract.read.name()
-    expect(contractName).to.equal('Contract')
+    expect(contractName).to.equal('Greg')
   })
 })
